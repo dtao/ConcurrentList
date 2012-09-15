75ba12a2478a4b6bc16b20bc103782df9b94e55c
Dan Tao <daniel.tao@gmail.com>
2012-09-15 09:23:54 -0700

    removing attempt to break List<T> for now

--- ConcurrentList.Tests/ConcurrentListTests.cs
+++ ConcurrentList.Tests/ConcurrentListTests.cs
@@ -54,45 +54,5 @@ namespace ConcurrentList.Tests
             Assert.That(list.Count, Is.EqualTo(10000));
             CollectionAssert.AreEquivalent(list, Enumerable.Range(0, 10000));
         }
-        
-        [Test]
-        public void SucceedsWhereListFails()
-        {
-            var list = new List<int>();
-            
-            using (var waitHandle = new ManualResetEvent(false))
-            {
-                var thread = new Thread(() =>
-                {
-                    while (!waitHandle.WaitOne(0))
-                    {
-                        // Just simulating multiple concurrent readers.
-                        foreach (int element in list)
-                        { }
-                    }
-                });
-                
-                thread.Start();
-                
-                var options = new ParallelOptions
-                {
-                    MaxDegreeOfParallelism = 10
-                };
-                
-                Parallel.For(0, 10, options, i =>
-                {
-                    for (int j = 0; j < 100; ++j)
-                    {
-                        list.Add((i * 100) + j);
-                    }
-                });
-                
-                waitHandle.Set();
-                thread.Join();
-            }
-            
-            Assert.That(list.Count, Is.EqualTo(1000));
-            CollectionAssert.AreEquivalent(list, Enumerable.Range(0, 1000));
-        }
     }
 }
